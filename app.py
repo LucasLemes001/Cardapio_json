78d2623150f68c14a2e12b4272224dfbf186ddda
LucasLemes001 <lucas.heine.lemes@icloud.com>
2023-06-03 16:34:54 -0300

    add lanches e valores a tabela Lista de produtos

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import sqlite3, sqlalchemy, json, flask
 
 
-# with sqlite3.connect('Lista_de_produtos.db') as conexao:
-    # banco_de_dados = conexao.cursor()
+with sqlite3.connect('Lista_de_produtos.db') as conexao:
+    banco_de_dados = conexao.cursor()
     # banco_de_dados.execute('create table Produtos(nome_do_produto text, ingrediente_produto text, valor_produto float);')
 
 # FAZER UMA SEQUENCIA ONDE VOU PEGAR
 #          NOME DO DOGAO / INGREDIENTES / VALOR
+'''
+for dicionario in cardapio:
+        
+        print(dicionario[>>>>'MUDAR AQUI A CHAVE PARA PEGAR INFORMAÇÃO'<<<<<])'''
 
 with open('novocardapio.json','r') as arquivo:
     cardapio = json.load(arquivo)
     for dicionario in cardapio:
-        print(dicionario['ingrediente'])
+        nome = dicionario["nome"]
+        ingrediente = None
+        valor = float(dicionario["preco"])
+        banco_de_dados.execute('insert into Produtos values(?,?,?)',[nome,ingrediente,valor])
+    conexao.commit()
         
